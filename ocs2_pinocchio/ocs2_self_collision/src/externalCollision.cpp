4470c26fa0afd0d154195ef8812ddee61186c5fd
Lin1225 <a3ie9981@gmail.com>
2023-01-12 21:25:20 +0800

    stable for static and moving obs with link4 and base_link

--- ocs2_pinocchio/ocs2_self_collision/src/externalCollision.cpp
+++ ocs2_pinocchio/ocs2_self_collision/src/externalCollision.cpp
@@ -122,13 +122,25 @@ vector_t externalCollision::getValue(const PinocchioInterface& pinocchioInterfac
   datavector.push_back(joint1Position);
   datavector.push_back(joint2Position);
   std::vector<double> distanceArray;
-  for (int i = 1; i < 2; i++)
-  {
-    double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
-                               +std::pow(datavector[i](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
-                               +std::pow(datavector[i](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
-    distanceArray.push_back(distance);  
-  }
+  // for (int i = 1; i < 2; i++)
+  // {
+  //   double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+  //                              +std::pow(datavector[i](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
+  //                              +std::pow(datavector[i](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
+  //   distanceArray.push_back(distance);  
+  // }
+
+  // base_link
+    double distance = std::sqrt(std::pow(datavector[0](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+                               +std::pow(datavector[0](1)-(obsData[1]+outObsData->twist.twist.linear.y),2));
+    distanceArray.push_back(distance); 
+
+  // link_4
+    distance = std::sqrt(std::pow(datavector[1](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+                        +std::pow(datavector[1](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
+                        +std::pow(datavector[1](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
+    distanceArray.push_back(distance);      
+
   // std::vector<double> distanceArray;
   // distanceArray.push_back(0.1);
   // distanceArray.push_back(0.1);
@@ -185,15 +197,27 @@ std::pair<vector_t, matrix_t> externalCollision::getLinearApproximation(const Pi
   // std::cout << outObsData->twist.twist.linear.x <<std::endl<<std::endl;
 
   std::vector<double> distanceArray;
-  for (int i = 1; i < 2; i++)
-  {
-    // double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]-0.5),2)+std::pow(datavector[i](1)-obsData[1],2)+std::pow(datavector[i](2)-obsData[2],2));
-    double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
-                               +std::pow(datavector[i](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
-                               +std::pow(datavector[i](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
+  // for (int i = 1; i < 2; i++)
+  // {
+  //   // double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]-0.5),2)+std::pow(datavector[i](1)-obsData[1],2)+std::pow(datavector[i](2)-obsData[2],2));
+  //   double distance = std::sqrt(std::pow(datavector[i](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+  //                              +std::pow(datavector[i](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
+  //                              +std::pow(datavector[i](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
                                
-    distanceArray.push_back(distance);  
-  }
+  //   distanceArray.push_back(distance);  
+  // }
+
+  // base_link
+    double distance = std::sqrt(std::pow(datavector[0](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+                               +std::pow(datavector[0](1)-(obsData[1]+outObsData->twist.twist.linear.y),2));
+    distanceArray.push_back(distance); 
+
+  // link_4
+    distance = std::sqrt(std::pow(datavector[1](0)-(obsData[0]+outObsData->twist.twist.linear.x),2)
+                        +std::pow(datavector[1](1)-(obsData[1]+outObsData->twist.twist.linear.y),2)
+                        +std::pow(datavector[1](2)-(obsData[2]+outObsData->twist.twist.linear.z),2));
+    distanceArray.push_back(distance);   
+
   // distanceArray.push_back(0.1);
   // distanceArray.push_back(0.1);
   // std::cout << "Done" << std::endl;
@@ -214,8 +238,10 @@ std::pair<vector_t, matrix_t> externalCollision::getLinearApproximation(const Pi
 
     // Jacobian calculation
     const auto& collisionPair = geometryModel.collisionPairs[i];
-    const auto& joint1 = geometryModel.geometryObjects[collisionPair.second].parentJoint;
-    const auto& joint2 = geometryModel.geometryObjects[collisionPair.first].parentJoint;
+    const auto& joint1 = geometryModel.geometryObjects[collisionPair.second].parentJoint; // link_4
+    const auto& joint2 = geometryModel.geometryObjects[collisionPair.first].parentJoint;  // base_link
+
+    // link_4
 
     // We need to get the jacobian of the point on the first object; use the joint jacobian translated to the point
     const vector3_t joint1Position = data.oMi[joint1].translation();
@@ -255,6 +281,23 @@ std::pair<vector_t, matrix_t> externalCollision::getLinearApproximation(const Pi
     // distanceVector.normalize();
 
     dfdq.row(i).noalias() = distanceVector.transpose() * differenceJacobian;
+
+    // base_link
+    ++i;
+    f[i] = distanceArray[i] - 0.4;
+
+    const vector3_t joint2Position = data.oMi[joint2].translation(); //arm tool0
+    matrix_t joint2Jacobian = matrix_t::Zero(6, model.nv);
+    pinocchio::getJointJacobian(model, data, joint2, pinocchio::ReferenceFrame::LOCAL_WORLD_ALIGNED, joint2Jacobian);
+    const matrix_t differenceJacobian2 = -joint2Jacobian;
+    
+    vector3_t distanceVector2 ; 
+    distanceVector2(0) = obsData[0] - joint2Position(0);
+    distanceVector2(1) = obsData[1] - joint2Position(1);
+    distanceVector2(2) = obsData[2] - joint2Position(2);
+
+
+    dfdq.row(i).noalias() = distanceVector2.transpose() * differenceJacobian2;
   }  // end of i loop
 
   return {f, dfdq};
